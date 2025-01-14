5aa18f269f6d98ae3044a5157d4d7ec540a8585c
TeodorFK <113384338+TeodorFK@users.noreply.github.com>
2025-01-14 10:58:28 +0100

    Add files via upload

---
+++ Oppgaver/Oppgave 3/Functions.js
@@ -0,0 +1,35 @@
+// Oppgave 1 //
+// Function skal gjøre noe eller lage en spesiel ting. kode som kan bukers om og om igjen//
+
+//Oppgave 2 //
+function greet(){
+    console.log ('Hello World!');
+}
+// navnet er greet og return returnerer verdien 'hello world' når function blir brukt. console.log(greet()); skriver ut greet function//
+// functionskall er å bruke functionen. Du kaller på function //
+greet();
+
+// Oppgave 3 //
+// parameter er når du lager funksjonen. i denne funksjonen er det colors //
+function colors(color){
+    console.log (color);
+}
+colors("blue")
+
+//argumenter er verdier som må sendes inn til funksjonen den kalles. Det du putter inn i () er argumentet. //
+
+// Oppgave 4 //
+
+function colors(color){
+    console.log (color);
+}
+colors("Min parameter er")
+colors("arumente er min")
+
+// Oppgave 5 //
+
+function idk(dont, know){
+console.log("Jeg fikk to parametere ", dont, "og", know)
+
+}
+idk ("dont know", "gg")
