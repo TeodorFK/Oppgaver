3e7c19e2c2d0cce5470a624ac5d699dc6de8d1ee
TeodorFK <113384338+TeodorFK@users.noreply.github.com>
2025-01-14 10:54:37 +0100

    Add files via upload

---
+++ Oppgaver/Oppgave 1-2/Script.js
@@ -0,0 +1,58 @@
+// Oppgave 1   Console.log() og tekst i Javascript //
+
+// A //
+
+console.log('Hello World!');
+
+// B //
+
+console.log('Jeg er trøtt før jeg skal på skolen.');
+
+//  C  //
+
+let text1 = "vann";
+let text2 = "melon";
+let result = text1.concat(text2);
+console.log(result)
+// En string concatenation er å legge sammen flere strings, sånn at det blir en ting tilsammen //
+
+// D //
+
+console.log('Geir Hilmersen er født i året' + 1979 + 42 +  'år gammel!');
+console.log(5+8)
+// stolker tallet som en string istedet for et tall //
+
+
+
+
+// Oppgave 2  Variabler//
+
+// A //
+// Gi navn til ett variabel . man kan bruke 'var', 'const' eller 'let'//
+
+// B //
+// Gir den en verdi for første gang //
+const teodor = 'kul'
+
+// C //
+// Å deklarere er å skrive en variabel og initialisere er og gi variabelen en verdi. //
+const skole = 'ikke bra'
+
+// D //
+// var en basically den gamle versionen av let. Let kan du bruke til å skifte verdien på en variabel etter du har gjort noe. //
+
+// E //
+const balleklapp = 200
+let airpods
+// forskjellen på konstant og variable er at npr du lager en konstant kan du ikke change verdien. variabel skal du ikke  gi den er verdi på starten//
+//du kan også change etterpå//
+
+// F //
+// objects, arrays, dates, maps og mer //
+
+// G //
+// Undefined har ikke noe verdi, Null gir en verdi men er ikke vert noe'
+let x
+console.log(x)
+x = null
+console.log(x)
